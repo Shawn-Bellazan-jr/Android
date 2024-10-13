2a73e35b733b0c0c570208fbaa6ffa4bb7d0dfcd
Shsawn Bellazan <shawn_bellazan@outlook.com>
2024-10-12 19:25:03 -0500

    Updates

--- directory-generator-plugin/build.gradle.kts
+++ directory-generator-plugin/build.gradle.kts
@@ -1,24 +1,35 @@
 plugins {
     `kotlin-dsl`
     `maven-publish`
-    signing
+    `java-gradle-plugin`
+    id("signing") // Correct plugin syntax for signing
 }
 
-group = "com.tooensure.io" // Replace with your group ID
+group = "com.tooensure.io.directory-generator" // Replace with your group ID
 version = "1.0.0" // Replace with your version
 
 dependencies {
     implementation(kotlin("stdlib"))
 }
+
+gradlePlugin {
+    plugins {
+        create("directoryGeneratorPlugin") {
+            id = "com.tooensure.io.directory-generator" // Plugin ID
+            implementationClass = "com.tooensure.DirectoryGeneratorPlugin" // Fully qualified name of the plugin class
+        }
+    }
+}
+
 publishing {
     publications {
         create<MavenPublication>("directoryGeneratorMaven") {
-            from(components["kotlin"])
+            from(components["kotlin"]) // Use "java" instead of "kotlin" for Gradle plugin publication
 
             pom {
                 name.set("Directory Generator Plugin")
-                description.set("A description of my plugin.")
-                url.set("https://github.com/Shawn-Bellazan-jr/Android/tree/master/app/src/main/java/com/example/android/ui")
+                description.set("A Gradle plugin to generate directories.")
+                url.set("https://github.com/Shawn-Bellazan-jr/Android")
 
                 licenses {
                     license {
@@ -37,13 +48,26 @@ publishing {
 
                 scm {
                     connection.set("scm:git:git://github.com/Shawn-Bellazan-jr/your-repo.git")
-                    developerConnection.set("scm:git:ssh://github.com/Shawn-Bellazan-jr/your-repo.git")
+                    developerConnection.set("scm:git:ssh://github.com/Shawn-Bellazan-jr/Android.git")
                     url.set("https://github.com/Shawn-Bellazan-jr/your-repo")
                 }
             }
         }
     }
+
+    repositories {
+        maven {
+            name = "GitHubPackages"
+            url = uri("https://jitpack.io")
+            credentials {
+                username = System.getenv("GITHUB_USERNAME") ?: ""
+                password = System.getenv("GITHUB_TOKEN") ?: "" // The token is used as the password
+            }
+        }
+    }
 }
+
 signing {
+    useGpgCmd() // This ensures the signing will use the GPG command-line tool
     sign(publishing.publications["directoryGeneratorMaven"])
 }
